32ccb010998f2d4ad2dc30fd65699ef0142e15f9
Zhaoyan Cong <congzhaoyan1999@gmail.com>
2024-02-14 21:14:21 +1100

    modified responsive design

--- src/components/WeatherCard/components/SearchHistory/components/City/City.jsx
+++ src/components/WeatherCard/components/SearchHistory/components/City/City.jsx
@@ -4,7 +4,7 @@ import TemperatureRange from "../../../../../TemperatureRange"
 
 const City = ({ nameValue, tempValue, imgUrl, bgColor, bgImg, onClick }) => {
     return (
-        <div className={`relative flex flex-col items-center justify-center rounded-3xl w-1/4 py-2 space-y-2 text-white shadow-md ${bgColor}
+        <div className={`relative flex flex-col items-center justify-center rounded-3xl w-1/4 h-full lg:h-2/3 py-2 space-y-2 text-white shadow-md ${bgColor}
             cursor-pointer transform transition duration-200 ease-in-out hover:scale-105 hover:shadow-xl`}
             onClick={() => onClick(nameValue)}>
             <div className="absolute top-0 left-0 w-full h-full bg-cover bg-center z-[-10]"
@@ -12,7 +12,7 @@ const City = ({ nameValue, tempValue, imgUrl, bgColor, bgImg, onClick }) => {
                          opacity: 0.25,
                          borderRadius: 'inherit' }}></div>
             <WeatherIcon imageUrl={imgUrl} className="w-12 h-12"/>
-            <Name value={nameValue} className="text-xl font-bold"/>
+            <Name value={nameValue} className="text-sm sm:text-md md:text-lg lg:text-xl font-bold"/>
             <TemperatureRange minTemp={tempValue.minTemp} maxTemp={tempValue.maxTemp} className="text-sm"/>
         </div>
     )
